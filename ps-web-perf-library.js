cd2188a870321c5bf108e9a3acf11d1009752173
Pavel Saman <samanpavel@gmail.com>
2021-12-01 11:21:37 +0100

    more metrics, bulk insert into Elastic

--- ps-web-perf-library.js
+++ ps-web-perf-library.js
@@ -26,16 +26,71 @@ function _getMetrics (perfEntries) {
 }
 
 function _getRequests (perfEntries) {
-  const requestStats = {
-    images : perfEntries.filter(pe => pe.name.match(regexMatchers.image)).length,
-    css    : perfEntries.filter(pe => pe.name.match(regexMatchers.css)).length,
-    scripts: perfEntries.filter(pe => pe.name.match(regexMatchers.scripts)).length,
-    fonts  : perfEntries.filter(pe => pe.name.match(regexMatchers.fonts)).length,
+  const requestResources = {
+    requests: perfEntries,
+    images  : perfEntries.filter(pe => pe.name.match(regexMatchers.image)),
+    css     : perfEntries.filter(pe => pe.name.match(regexMatchers.css)),
+    scripts : perfEntries.filter(pe => pe.name.match(regexMatchers.scripts)),
+    fonts   : perfEntries.filter(pe => pe.name.match(regexMatchers.fonts)),
   };
 
+  let requestLength = {
+    requests: requestResources.requests.length,
+    images  : requestResources.images.length,
+    css     : requestResources.css.length,
+    scripts : requestResources.scripts.length,
+    fonts   : requestResources.fonts.length,
+  };
+  requestLength.other = requestLength.requests
+    - requestLength.images
+    - requestLength.css
+    - requestLength.scripts
+    - requestLength.fonts;
+
+  let requestTransferSize = {
+    requestsTransferSize: requestResources.requests.map(pe => pe.transferSize || 0).reduce((a, b) => a + b, 0),
+    imagesTransferSize  : requestResources.images.map(pe => pe.transferSize || 0).reduce((a, b) => a + b, 0),
+    cssTransferSize     : requestResources.css.map(pe => pe.transferSize || 0).reduce((a, b) => a + b, 0),
+    scriptsTransferSize : requestResources.scripts.map(pe => pe.transferSize || 0).reduce((a, b) => a + b, 0),
+    fontsTransferSize   : requestResources.fonts.map(pe => pe.transferSize || 0).reduce((a, b) => a + b, 0),
+  };
+  requestTransferSize.otherTransferSize = requestTransferSize.requestsTransferSize
+    - requestTransferSize.imagesTransferSize
+    - requestTransferSize.cssTransferSize
+    - requestTransferSize.scriptsTransferSize
+    - requestTransferSize.fontsTransferSize;
+
+  let requestEncodedSize = {
+    requestsEncodedSize: requestResources.requests.map(pe => pe.encodedBodySize || 0).reduce((a, b) => a + b, 0),
+    imagesEncodedSize  : requestResources.images.map(pe => pe.encodedBodySize || 0).reduce((a, b) => a + b, 0),
+    cssEncodedSize     : requestResources.css.map(pe => pe.encodedBodySize || 0).reduce((a, b) => a + b, 0),
+    scriptsEncodedSize : requestResources.scripts.map(pe => pe.encodedBodySize || 0).reduce((a, b) => a + b, 0),
+    fontsEncodedSize   : requestResources.fonts.map(pe => pe.encodedBodySize || 0).reduce((a, b) => a + b, 0),
+  };
+  requestEncodedSize.otherEncodedSize = requestEncodedSize.requestsEncodedSize
+    - requestEncodedSize.imagesEncodedSize
+    - requestEncodedSize.cssEncodedSize
+    - requestEncodedSize.scriptsEncodedSize
+    - requestEncodedSize.fontsEncodedSize;
+
+  let requestDecodedSize = {
+    requestsDecodedSize: requestResources.requests.map(pe => pe.decodedBodySize || 0).reduce((a, b) => a + b, 0),
+    imagesDecodedSize  : requestResources.images.map(pe => pe.decodedBodySize || 0).reduce((a, b) => a + b, 0),
+    cssDecodedSize     : requestResources.css.map(pe => pe.decodedBodySize || 0).reduce((a, b) => a + b, 0),
+    scriptsDecodedSize : requestResources.scripts.map(pe => pe.decodedBodySize || 0).reduce((a, b) => a + b, 0),
+    fontsDecodedSize   : requestResources.fonts.map(pe => pe.decodedBodySize || 0).reduce((a, b) => a + b, 0),
+  };
+  requestEncodedSize.otherDecodedSize = requestDecodedSize.requestsDecodedSize
+    - requestDecodedSize.imagesDecodedSize
+    - requestDecodedSize.cssDecodedSize
+    - requestDecodedSize.scriptsDecodedSize
+    - requestDecodedSize.fontsDecodedSize;
+
   return {
-    ...requestStats,
-    ...{ other: perfEntries.length - Object.keys(requestStats).map(k => requestStats[k]).reduce((a, b) => a + b, 0) },
+    ...requestLength,
+    ...requestTransferSize,
+    ...requestEncodedSize,
+    ...requestDecodedSize,
   };
 }
 
@@ -48,8 +103,20 @@ async function exportWebPerfStats (perfEntries) {
     datetime       : now.toISOString(),
   };
 
-  await axios.post(`${elasticUrl}:${elasticPort}/ps-metrics/_doc/?pretty`, { ..._getMetrics(perfEntries), ...meta });
-  await axios.post(`${elasticUrl}:${elasticPort}/ps-requests/_doc/?pretty`, { ..._getRequests(perfEntries), ...meta });
+  const psMetricsIndex = '{ "index":{ "_index": "ps-metrics", "_type": "_doc" } }';
+  const psRequestsIndex = '{ "index":{ "_index": "ps-requests", "_type": "_doc" } }';
+
+  const psMetricsPlain = JSON.stringify({ ..._getMetrics(perfEntries), ...meta });
+  const psRequestsPlain = JSON.stringify({ ..._getRequests(perfEntries), ...meta });
+
+  let bulkPayload = `${psMetricsIndex}\n${psMetricsPlain}\n`;
+  bulkPayload = `${bulkPayload}${psRequestsIndex}\n${psRequestsPlain}\n`;
+
+  await axios.post(`${elasticUrl}:${elasticPort}/_bulk`, bulkPayload, {
+    headers: {
+      'content-type': 'application/x-ndjson',
+    }
+  });
 
   return true;
 }
