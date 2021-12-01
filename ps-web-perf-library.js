a8e90bfe5e5aadfefc32d44befd163e541d47ee9
Pavel Saman <samanpavel@gmail.com>
2021-12-01 15:54:44 +0100

    finish basic functionality

--- ps-web-perf-library.js
+++ ps-web-perf-library.js
@@ -11,21 +11,30 @@ const regexMatchers = {
   fonts  : /(.woff|.woff2)$/,
 };
 
-function _getMetrics (perfEntries) {
+const indexes = {
+  psMetricsIndex    : '{ "index":{ "_index": "ps-metrics", "_type": "_doc" } }',
+  psRequestsIndex   : '{ "index":{ "_index": "ps-requests", "_type": "_doc" } }',
+  psPerfEntriesIndex: '{ "index":{ "_index": "ps-perf-entries", "_type": "_doc" } }',
+};
+
+function _getMetrics (perfEntries, meta) {
   const metrics = {
     fp  : perfEntries.filter(pe => pe.name === 'first-paint'),
     fcp : perfEntries.filter(pe => pe.name === 'first-contentful-paint'),
     ttfb: perfEntries[0].responseStart - perfEntries[0].requestStart,
   };
 
-  return {
-    fp  : metrics.fp.length ? metrics.fp[0].startTime : 0,
-    fcp : metrics.fcp.length ? metrics.fcp[0].startTime : 0,
-    ttfb: metrics.ttfb,
-  };
+  return [{
+    ...{
+      fp  : metrics.fp.length ? metrics.fp[0].startTime : 0,
+      fcp : metrics.fcp.length ? metrics.fcp[0].startTime : 0,
+      ttfb: metrics.ttfb,
+    },
+    ...meta,
+  }];
 }
 
-function _getRequests (perfEntries) {
+function _getRequests (perfEntries, meta) {
   const requestResources = {
     requests: perfEntries,
     images  : perfEntries.filter(pe => pe.name.match(regexMatchers.image)),
@@ -34,7 +43,7 @@ function _getRequests (perfEntries) {
     fonts   : perfEntries.filter(pe => pe.name.match(regexMatchers.fonts)),
   };
 
-  let requestLength = {
+  const requestLength = {
     requests: requestResources.requests.length,
     images  : requestResources.images.length,
     css     : requestResources.css.length,
@@ -47,7 +56,7 @@ function _getRequests (perfEntries) {
     - requestLength.scripts
     - requestLength.fonts;
 
-  let requestTransferSize = {
+  const requestTransferSize = {
     requestsTransferSize: requestResources.requests.map(pe => pe.transferSize || 0).reduce((a, b) => a + b, 0),
     imagesTransferSize  : requestResources.images.map(pe => pe.transferSize || 0).reduce((a, b) => a + b, 0),
     cssTransferSize     : requestResources.css.map(pe => pe.transferSize || 0).reduce((a, b) => a + b, 0),
@@ -60,7 +69,7 @@ function _getRequests (perfEntries) {
     - requestTransferSize.scriptsTransferSize
     - requestTransferSize.fontsTransferSize;
 
-  let requestEncodedSize = {
+  const requestEncodedSize = {
     requestsEncodedSize: requestResources.requests.map(pe => pe.encodedBodySize || 0).reduce((a, b) => a + b, 0),
     imagesEncodedSize  : requestResources.images.map(pe => pe.encodedBodySize || 0).reduce((a, b) => a + b, 0),
     cssEncodedSize     : requestResources.css.map(pe => pe.encodedBodySize || 0).reduce((a, b) => a + b, 0),
@@ -73,7 +82,7 @@ function _getRequests (perfEntries) {
     - requestEncodedSize.scriptsEncodedSize
     - requestEncodedSize.fontsEncodedSize;
 
-  let requestDecodedSize = {
+  const requestDecodedSize = {
     requestsDecodedSize: requestResources.requests.map(pe => pe.decodedBodySize || 0).reduce((a, b) => a + b, 0),
     imagesDecodedSize  : requestResources.images.map(pe => pe.decodedBodySize || 0).reduce((a, b) => a + b, 0),
     cssDecodedSize     : requestResources.css.map(pe => pe.decodedBodySize || 0).reduce((a, b) => a + b, 0),
@@ -86,12 +95,53 @@ function _getRequests (perfEntries) {
     - requestDecodedSize.scriptsDecodedSize
     - requestDecodedSize.fontsDecodedSize;
 
-  return {
+  return [{
     ...requestLength,
     ...requestTransferSize,
     ...requestEncodedSize,
     ...requestDecodedSize,
-  };
+    ...meta,
+  }];
+}
+
+function _getPerfEntries (perfEntries, meta) {
+  const clsPerfEntries = perfEntries.filter(pe => pe.entryType !== 'paint');
+  const resultPerfEntries = [];
+
+  clsPerfEntries.forEach((pe, i) => {
+    resultPerfEntries.push({
+      ...{
+        transferSize   : pe.transferSize,
+        encodedBodySize: pe.encodedBodySize,
+        decodedBodySize: pe.decodedBodySize,
+        dns            : pe.domainLookupEnd - pe.domainLookupStart,
+        domComplete    : pe.domComplete,
+        domInteractive : pe.domInteractive,
+        entryType      : pe.entryType,
+        headerSize     : pe.transferSize - pe.encodedBodySize,
+        name           : pe.name,
+        redirectTime   : pe.redirectEnd - pe.redirectStart,
+        response       : pe.responseEnd - pe.requestStart,
+        ttfb           : pe.responseStart - pe.requestStart,
+        initConn       : pe.connectEnd - pe.connectStart,
+        load           : pe.loadEventEnd,
+        uuid           : uuid.v4(),
+        sequence       : i,
+      },
+      ...meta,
+    });
+  });
+
+  return resultPerfEntries;
+}
+
+function _getPlain (arr, index) {
+  let resultPlain = '';
+  arr.forEach(e => {
+    resultPlain = `${resultPlain}\n${index}\n${JSON.stringify(e)}\n`;
+  });
+
+  return resultPlain;
 }
 
 async function exportWebPerfStats (perfEntries) {
@@ -100,18 +150,14 @@ async function exportWebPerfStats (perfEntries) {
   const now = new Date();
   const meta = {
     uuidPerfEntries: uuid.v4(),
-    datetime       : now.toISOString(),
+    timestamp      : now.toISOString(),
   };
 
-  const psMetricsIndex = '{ "index":{ "_index": "ps-metrics", "_type": "_doc" } }';
-  const psRequestsIndex = '{ "index":{ "_index": "ps-requests", "_type": "_doc" } }';
+  const psMetricsPlain = _getPlain(_getMetrics(perfEntries, meta), indexes.psMetricsIndex);
+  const psRequestsPlain = _getPlain(_getRequests(perfEntries, meta), indexes.psRequestsIndex);
+  const psPerfEntriesPlain = _getPlain(_getPerfEntries(perfEntries, meta), indexes.psPerfEntriesIndex);
 
-  const psMetricsPlain = JSON.stringify({ ..._getMetrics(perfEntries), ...meta });
-  const psRequestsPlain = JSON.stringify({ ..._getRequests(perfEntries), ...meta });
-
-  let bulkPayload = `${psMetricsIndex}\n${psMetricsPlain}\n`;
-  bulkPayload = `${bulkPayload}${psRequestsIndex}\n${psRequestsPlain}\n`;
-
+  const bulkPayload = `${psMetricsPlain}${psRequestsPlain}${psPerfEntriesPlain}`;
   await axios.post(`${elasticUrl}:${elasticPort}/_bulk`, bulkPayload, {
     headers: {
       'content-type': 'application/x-ndjson',
